2ebcfb6a277df61f7dddc73eaf453ea323407194
huunghiaa <nghuunghiaa@gmail.com>
2019-04-08 15:16:22 +0700

    Initial Commit

---
+++ messagingapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  messagingapp
+//
+//  Created by Nghia Nguyen Huu on 4/8/19.
+//  Copyright © 2019 Nghia Nguyen Huu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
